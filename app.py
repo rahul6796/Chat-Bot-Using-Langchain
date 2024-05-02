9a0534162769077f78db684b323fdcbfb4d60031
Rahul Parjapati <60359760+rahul6796@users.noreply.github.com>
2024-05-03 00:29:26 +0530

    app.py
    implemented first part of this chat bot

--- app.py
+++ app.py
@@ -9,9 +9,34 @@ from dotenv import load_dotenv
 load_dotenv()
 
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+
+# langsmith for tracking:
 os.environ['LANGCHAIN_TRACING_V2'] = 'true'
 os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
 
 
 
 # prompt template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please response to the user queries"),
+        ("user", "Question : {question}")
+    ]
+)
+
+
+# streamlit
+
+st.title('Chat Bot')
+input_txt = st.text_input('Search')
+
+
+# open ai llm
+
+llm = ChatOpenAI(model='gpt-3.5-turbo')
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_txt:
+    st.write(chain.invoke({'question': input_txt }))
