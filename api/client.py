144a399bd55c1465e001a4d0a15b69c129ab6138
Rahul Parjapati <60359760+rahul6796@users.noreply.github.com>
2024-05-04 12:25:48 +0530

    client.py
    created streamlit clinet which is interact with app with is llm models and given responce back to streamlit ui

--- api/client.py
+++ api/client.py
@@ -1,1 +1,23 @@
 
+import requests
+import streamlit as st
+
+
+# calling api
+
+def get_api_response(input_text):
+    response = requests.post("http://localhost:8000/essay_ollama_model/invoke",
+                             json= {'input': {'topic': input_text}})
+    
+    return response.json()['output']
+
+# calling streamlit
+st.title('Chat-Bot Using Gemma Model !')
+input_text = st.text_input('Write an easy essay')
+
+
+if input_text:
+    st.write(get_api_response(input_text=input_text))
+    
+
+
