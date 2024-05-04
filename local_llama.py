c0321fd6fe9cad53caa0227ed2faf16595cf5b43
Rahul Parjapati <60359760+rahul6796@users.noreply.github.com>
2024-05-04 11:41:45 +0530

    local_llama.py
    added code in

--- local_llama.py
+++ local_llama.py
@@ -1,6 +1,6 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms.ollama import Ollama
+from langchain_community.llms import Ollama
 
 
 import os 
